a6867663e7026565b07a6364027cbeb1e5fb3c56
donfortune <donfortunet.df@gmail.com>
2023-11-27 11:36:17 -0800

    added commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: the linked list
+ * Return: return 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise;
+	listint_t *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	tortoise = list;
+	hare = list->next;
+
+	while (tortoise != hare)
+	{
+		if (hare == NULL || hare->next == NULL)
+			return (0);
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+
+	return (1);
+}
