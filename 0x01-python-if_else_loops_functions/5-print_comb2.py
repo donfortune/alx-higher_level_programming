6f64cf4f79906a82894b6100c287101daf4c623c
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-11-28 12:39:56 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 99):
-     print(f"{i:02d}", end=", " if i < 99 else "\n",)
+for i in range(100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print(i)
