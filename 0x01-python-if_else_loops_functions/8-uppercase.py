9fe8984b7d4fdeed5d4ab9fbb8154571aa3a071d
donfortune <donfortunet.df@gmail.com>
2023-11-28 10:22:25 -0800

    new commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
    for i in range(len(str)):
        char = ord(str[i])
-        if 97 <= char <= 122:
+
+       if 97 <= char <= 122:
             char = char - 32
-        print("{:c}".format(char), end="")
-    print()
+
+            print("{:c}".format(char), end="")
