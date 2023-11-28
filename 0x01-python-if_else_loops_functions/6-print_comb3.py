735661d0216629631d2fe026a9d4c4e9307012f9
donfortune <donfortunet.df@gmail.com>
2023-11-28 06:22:08 -0800

    new file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i < 99 else "\n",)
+for num1 in range(10):
+    for num2 in range(10):
+        if num2 > num1 and num1 != 8:
+            print("{}{}".format(num1, num2), end=", ")
+        elif num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+            break
