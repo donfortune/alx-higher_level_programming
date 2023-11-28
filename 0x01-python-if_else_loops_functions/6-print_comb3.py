fff5a9e0abef05668b80fddbcd6da5cd9281fe73
donfortune <donfortunet.df@gmail.com>
2023-11-28 06:01:01 -0800

    new commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(99):
-     print(f"{i:02d}", end=", " if i < 99 else "\n",)
+for i in range(9):
+    for j in range(i + 1, 10):
+        print(f"{i}{j}", end=", " if i < 99 else "\n",)
