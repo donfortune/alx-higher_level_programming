4406f59ebc8c2669b8d8fb35454976985e5c45ed
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-11-30 13:18:44 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-from calculator_1 import add,sub,mul,div
+if __name__ == "__main__":
+    from calculator_1 import add,sub,mul,div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-result = add(a,b)
-result_1 = sub(a,b)
-result_2 = mul(a,b)
-result_3 = div(a,b)
-print(f"{a} + {b} = {result}")
-print(f"{a} - {b} = {result_1}")
-print(f"{a} * {b} = {result_2}")
-print(f"{a} / {b} = {result_3}")
+    result = add(a,b)
+    result_1 = sub(a,b)
+    result_2 = mul(a,b)
+    result_3 = div(a,b)
+    print(f"{a} + {b} = {result}")
+    print(f"{a} - {b} = {result_1}")
+    print(f"{a} * {b} = {result_2}")
+    print(f"{a} / {b} = {result_3}")
 
 
