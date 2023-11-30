0336a031df3dd91304c3ad52a25bce57801bb386
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-11-30 13:29:54 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-def arg_count(argc, argv):
-    if argc == 1:
-        print('1 argument:')
-        print(f"1: {argv[0]}")
-    elif argc > 1:
-        print(f"{argc} arguments:")
-        for i, arg in enumerate(argv, 1):
-            print(f"{i}: {arg}")
-    else:
-        print("No arguments provided.")
+    def arg_count(argc, argv):
+        if argc == 1:
+            print('1 argument:')
+            print(f"1: {argv[0]}")
+        elif argc > 1:
+            print(f"{argc} arguments:")
+            for i, arg in enumerate(argv, 1):
+                print(f"{i}: {arg}")
+        else:
+            print("No arguments provided.")
 
 argc = len(sys.argv) - 1
 argv = sys.argv[1:]
