ea329da0b360380e2e9444a00533780a7267ac17
donfortune <donfortunet.df@gmail.com>
2023-11-30 04:51:01 -0800

    added files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
             print('1 argument:')
             print("1: {}".format(argv[0]))
         elif argc > 1:
-            rint("{} arguments:".format(argc)))
+            print("{} arguments:".format(argc))
             for i, arg in enumerate(argv, 1):
                 print("{}: {}".format(i, arg))
         else:
             print("No arguments provided.")
 
-argc = len(sys.argv) - 1
-argv = sys.argv[1:]
-arg_count(argc, argv)
+    argc = len(sys.argv) - 1
+    argv = sys.argv[1:]
+    arg_count(argc, argv)
