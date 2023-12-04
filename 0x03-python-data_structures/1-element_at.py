b6922dd02a26f0619d47eef3743db063f946dde0
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-12-04 17:25:52 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,4 @@
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    else:
-        return my_list[idx]
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
