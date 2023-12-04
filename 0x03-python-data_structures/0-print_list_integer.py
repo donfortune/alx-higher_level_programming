b76324b2d681ad089fe79a131ea03a34984de207
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-12-04 17:22:23 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-def print_list_integer(my_list=None):
-    if my_list is None:
-        my_list = []
-    for item in my_list:
-        print(item)
-
-print_list_integer([1, 2, 3, 4, 5])
-
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
