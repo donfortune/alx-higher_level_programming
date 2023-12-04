b12e33044a5216d9c6b38f2d1572687fee7b343d
donfortune <donfortunet.df@gmail.com>
2023-12-04 08:15:51 -0800

    new files

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=None):
+    if my_list is None:
+        my_list = []
+    for item in my_list:
+        print(item)
+
+print_list_integer([1, 2, 3, 4, 5])
