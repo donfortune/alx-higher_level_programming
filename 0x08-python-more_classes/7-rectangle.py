38117635be8ba691e1236fd78aa576b138fd2336
donfortune <donfortunet.df@gmail.com>
2024-01-04 07:14:56 -0800

    added file

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ class rectangle"""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            return '\n'.join(['#' * self.width for _ in range(self.height)])
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print('Bye rectangle...')
