ff31a5703e4f93dcce9a8c19692b6ed3f8bc83d6
donfortune <donfortunet.df@gmail.com>
2024-01-08 00:16:26 -0800

    added file

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return f"[Rectangle] {self.width}/{self.height}"
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return f"[Square] {self.width}/{self.height}"
